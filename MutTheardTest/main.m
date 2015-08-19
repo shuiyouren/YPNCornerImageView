6b856804c737c612cbbbd42e45fa65b5705ae803
shuiyouren <shuiyouren@gmail.com>
2015-08-19 19:26:09 +0800

    Initial Commit

---
+++ MutTheardTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MutTheardTest
+//
+//  Created by syr on 15/8/19.
+//  Copyright (c) 2015年 syr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
